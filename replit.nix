b622d1e386415be65e08e7fba50d38801f1c4b96
Nothing <73119361+findme-19@users.noreply.github.com>
2023-08-06 15:07:41 +0700

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,20 +1,12 @@
 { pkgs }: {
-    deps = [
-        pkgs.arcan.ffmpeg
-        pkgs.nodejs-16_x
-        pkgs.openssh_with_kerberos
-        pkgs.nodePackages.typescript
-        pkgs.nodePackages.pm2
-        pkgs.imagemagick
-        pkgs.libwebp
-        pkgs.git
-        pkgs.python2
-        pkgs.python310Packages.python
-        pkgs.libuuid
-    ];
-    env = {
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-            pkgs.libuuid
-        ];
-    };
+	deps = [
+		pkgs.neofetch
+		pkgs.imagemagick
+		pkgs.nodejs-19_x
+		pkgs.speedtest-cli
+		pkgs.jellyfin-ffmpeg
+		pkgs.git
+		pkgs.python2
+		pkgs.python310Packages.python
+	];
 }
