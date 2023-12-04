2a184b6f52ace7cc9d4e8a08b93ef25c82e4d69a
Zeiyy <81281058+xznsenpai@users.noreply.github.com>
2023-12-05 07:24:32 +0800

    Fix

--- plugins/weebs/weebs-pinterest.js
+++ plugins/weebs/weebs-pinterest.js
@@ -1,20 +1,37 @@
 var handler = async (m, {
+	command,
+	args,
 	text,
-	conn
+	usedPrefix
 }) => {
-	if (!text) return m.reply('apakah....')
+	if (!text) throw 'Nyari foto apa?'
+	let [t1, t2] = text.split('|');
 	try {
-		var response = await fetch(API('xzn', 'api/pinterest', {
+		var api = await (await fetch(API('xzn', 'api/pinterest', {
 			search: text
-		}, 'apikey'))
-		var wtf = await response.json()
-		if (!Array.isArray(wtf)) throw wtf
-		return await conn.sendFile(m.chat, wtf.getRandom().media.url, '', text, m)
-	} catch (e) {
-		m.reply("mana gak ada")
-	};
-};
-handler.help = handler.command = ['pinterest'];
-handler.tags = ['weebs'];
+		}, 'apikey'))).json();
 
-export default handler;
+		if (t2 && !isNaN(t2)) {
+			const limit = Math.min(t2, api.length);
+			for (let i = 0; i < limit; i++) {
+				let res = api[i];
+				conn.sendFile(m.chat, res.media.url, '', '', m);
+				await delay(1000);
+			}
+		} else {
+			if (!t2) { // changed from !text to !t2
+				var res = api.getRandom()
+				conn.sendFile(m.chat, res.media.url, '', `*${res.title}*\n\nInfo:\nWidth: ${res.media.width}\nHeight: ${res.media.height}\n\n_${res.created_at}_`, m);
+			} else {
+				throw 'Format input tidak valid. Silakan masukkan jumlah yang benar.';
+			}
+		}
+	} catch (e) {
+		throw `Gagal mendapatkan Foto,\nError: ` + e.toString();
+	}
+};
+
+handler.help = ['pinterest'];
+handler.command = ['pin', 'pinterest'];
+handler.tags = ['weebs'];
+module.exports = handler;
