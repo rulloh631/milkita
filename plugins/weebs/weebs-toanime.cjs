03e087aa09a9de1f7dee3a06a0bc749c2e16772a
Zeiyy <81281058+xznsenpai@users.noreply.github.com>
2023-12-05 07:25:06 +0800

    Fix

--- plugins/weebs/weebs-toanime.cjs
+++ plugins/weebs/weebs-toanime.cjs
@@ -9,6 +9,7 @@ var handler = async (m, {
 		var q = m.quoted ? m.quoted : m,
 			mime = (q.msg || q).mimetype || ''
 		if (/image/g.test(mime)) {
+		  m.reply('*p l e a s e  w a i t. . .*')
 			var res = await fetch(API('xzn', 'api/toanime', {
 				url: await uploadFile(await q.download())
 			}, 'apikey'));
